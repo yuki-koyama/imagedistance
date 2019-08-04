acd856efc7e9ddf856c04f7683176c6e1ecd092f
Yuki Koyama <yuki@koyama.xyz>
2019-08-04 15:37:19 +0900

    Add a function to create histogram

--- src/imagedistance.cpp
+++ src/imagedistance.cpp
@@ -1,5 +1,43 @@
 #include <imagedistance.hpp>
 
+namespace imagedistance
+{
+    namespace internal
+    {
+        Histogram CalcHistogram(int num_bins, const Eigen::MatrixXd& channel);
+    }
+}
+
+imagedistance::Histogram imagedistance::internal::CalcHistogram(const int              num_bins,
+                                                                const Eigen::MatrixXd& channel)
+{
+    Histogram histogram  = Histogram::Zero(num_bins);
+
+    const int w = channel.cols();
+    const int h = channel.rows();
+
+    // Note: the following double loop is probably able to be implemented using element-wise operations
+
+    for (int x = 0; x < w; ++ x)
+    {
+        for (int y = 0; y < h; ++ y)
+        {
+            const double& value = channel(y, x);
+
+            for (unsigned i = 0; i < num_bins; ++ i)
+            {
+                if (value <= (static_cast<double>(i + 1) / static_cast<double>(num_bins)))
+                {
+                    histogram(i) += 1.0;
+                    break;
+                }
+            }
+        }
+    }
+
+    return histogram / static_cast<double>(w * h);
+}
+
 imagedistance::ImageDistanceObject::ImageDistanceObject(const Eigen::MatrixXd& r_channel,
                                                         const Eigen::MatrixXd& g_channel,
                                                         const Eigen::MatrixXd& b_channel,

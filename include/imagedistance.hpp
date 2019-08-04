0e78b3b663e2ecb90278d277a40832782b764258
Yuki Koyama <yuki@koyama.xyz>
2019-08-04 15:16:23 +0900

    Add basic class definition

--- include/imagedistance.hpp
+++ include/imagedistance.hpp
@@ -1,4 +1,26 @@
 #ifndef IMAGEDISTANCE_HPP
 #define IMAGEDISTANCE_HPP
 
+#include <array>
+#include <Eigen/Core>
+
+namespace imagedistance
+{
+    using Histogram = Eigen::VectorXd;
+
+    class ImageDistanceObject
+    {
+    public:
+        ImageDistanceObject(const Eigen::MatrixXd& r_channel,
+                            const Eigen::MatrixXd& g_channel,
+                            const Eigen::MatrixXd& b_channel);
+
+    private:
+        std::array<Histogram, 3> m_rgb_histograms;
+        std::array<Histogram, 3> m_hsl_histograms;
+        Histogram                m_intensity_histogram;
+        std::array<Histogram, 2> m_edge_histograms;
+    };
+}
+
 #endif /* IMAGEDISTANCE_HPP */
